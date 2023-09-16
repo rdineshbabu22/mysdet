3e7f7d8e4731b5fce3a5865495cf7f280c9feda3
Dinesh <rdineshbabu22@gmail.com>
2023-09-16 08:36:19 +0530

    my java homework

--- src/main/java/sdet/javafoundationexercise/ReverseString.java
+++ src/main/java/sdet/javafoundationexercise/ReverseString.java
@@ -1,24 +1,51 @@
 package sdet.javafoundationexercise;
 
+import org.junit.Test;
+
+import junit.framework.Assert;
+
 public class ReverseString {
-	   public String reverseWords(String s) {
+	public String reverseWords(String s) {
+		StringBuilder reverseStr = new StringBuilder();
+		int beginIndex = 0, indexOfSpace = 0;
 
-		   char[] reverseCharArray= new char[s.length()];
-		   String[] stringArray = s.split("");
-		   int count=0;
+		while (s.indexOf(' ', beginIndex) != -1)
 
-		   for (int i =0;i<stringArray.length;i++)
+		{
+			System.out.println("Index of space is "+indexOfSpace);
+			indexOfSpace = s.indexOf(" ", beginIndex);
+			for (int i = indexOfSpace-1; i >= beginIndex; i--)
 
-		   {
+			{
+				reverseStr.append(s.charAt(i));
+			}
+			reverseStr.append(' ');
+			beginIndex = indexOfSpace+1;
+			System.out.println("New Begin "+beginIndex);
 
-		   reverseCharArray[count]=s.charAt(i);
-		   count++;
+		}
 
-		   }
+		if (s.indexOf(' ', beginIndex) == -1) {
 
-		   String output =new String(reverseCharArray);
-		   return output;
-		           
-		       }
+			for (int i = s.length() - 1; i >= beginIndex; i--)
+
+			{
+				reverseStr.append(s.charAt(i));
+			}
+
+		}
+
+		return reverseStr.toString();
+	}
+	
+	@Test
+	public void verifySc1() {
+		Assert.assertEquals("ih", reverseWords("hi"));
+	}
+	
+	@Test
+	public void verifySc2() {
+		Assert.assertEquals("ih olleh ", reverseWords("hi hello"));
+	}
 
 }
